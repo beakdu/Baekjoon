2919527be17083cad6e3ec569ae069018aa9dbf5
LeeJungHyun <167539016+beakdu@users.noreply.github.com>
2024-09-02 09:10:53 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+X= int(input())
+
+if 90<=X and X <=100:
+    print('A')
+elif 80<= X and X <=89:
+    print('B')
+elif 70 <= X and X <=79:
+    print('C')
+elif 60 <= X and X <=69:
+    print('D')
+else:
+    print('F')
+    
